b3855ad2ab07ea88555953f891b0faa562da5245
AbbieN <52113711+Abbie-n@users.noreply.github.com>
2022-11-22 12:29:46 +0100

    bugfix: translation, responsiveness

--- lib/features/presentation/aac/widget/aac_single_item.dart
+++ lib/features/presentation/aac/widget/aac_single_item.dart
@@ -10,71 +10,76 @@ class AACSingleItem extends StatelessWidget {
     required this.image,
     required this.country,
     required this.value,
+    required this.onTap,
   });
 
   final String name;
   final String image;
   final String country;
   final String value;
+  final Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 16,
-        vertical: 10,
-      ),
-      child: Row(
-        children: [
-          CachedNetworkImage(
-            placeholder: (context, url) => const Center(
-              child: CircularLoadingWidget(height: 50),
-            ),
-            errorWidget: (context, url, _) => const SizedBox.shrink(),
-            imageUrl: image,
-            imageBuilder: (context, imageProvider) => Container(
-              height: 100,
-              width: 120,
-              padding: const EdgeInsets.symmetric(horizontal: 9),
-              child: Image.network(
-                image,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 10,
+        ),
+        child: Row(
+          children: [
+            CachedNetworkImage(
+              placeholder: (context, url) => const Center(
+                child: CircularLoadingWidget(height: 50),
+              ),
+              errorWidget: (context, url, _) => const SizedBox.shrink(),
+              imageUrl: image,
+              imageBuilder: (context, imageProvider) => Container(
+                height: 100,
+                width: 100,
+                padding: const EdgeInsets.symmetric(horizontal: 9),
+                child: Image.network(
+                  image,
+                ),
               ),
             ),
-          ),
-          const XMargin(15),
-          SizedBox(
-            width: MediaQuery.of(context).size.width / 1.75,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextWidget(
-                  name,
-                  fontSize: 20,
-                  textColor: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-                const YMargin(5),
-                TextWidget(
-                  country,
-                  fontSize: 14,
-                  textColor: AppColors.grey,
-                  fontWeight: FontWeight.w400,
-                ),
-                const YMargin(5),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextWidget(
-                    '$value ${context.l10n.millions}',
-                    fontSize: 18,
-                    textColor: AppColors.darkText.withOpacity(.8),
-                    fontWeight: FontWeight.w500,
-                    textAlign: TextAlign.end,
+            const XMargin(5),
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 1.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextWidget(
+                    name,
+                    fontSize: 20,
+                    textColor: Colors.black,
+                    fontWeight: FontWeight.bold,
                   ),
-                ),
-              ],
+                  const YMargin(5),
+                  TextWidget(
+                    country,
+                    fontSize: 14,
+                    textColor: AppColors.grey,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  const YMargin(5),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextWidget(
+                      '$value ${context.l10n.millions}',
+                      fontSize: 18,
+                      textColor: AppColors.darkText.withOpacity(.8),
+                      fontWeight: FontWeight.w500,
+                      textAlign: TextAlign.end,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
